8db64469b82710901ac1757c9ea907373af00ede
Sunil.Jacob <sunil1405@gmail.com>
2019-07-03 20:25:10 +0530

    Added new tests

--- randomness/tests/test_random_choice.py
+++ randomness/tests/test_random_choice.py
@@ -2,16 +2,20 @@ from randomness.src import random_choice as rand, generate_deck_of_cards as card
 # from randomness.src.random_choice import deck
 def test_random_with_seed():
     seed = rand.basic_use_of_rand()
-    assert seed == '9C'
+    assert seed == '9C', 'seed wrong'
 
 def test_random_shuffle_function():
     shuffle_list = rand.random_with_shuffle()
-    assert 'J' in shuffle_list
+    assert 'J' in shuffle_list, 'wrong shuffle list'
 
 def test_generate_deck_of_cards():
     card = cards.generate_deck_of_cards()
-    assert '2D' in card
+    assert '2D' in card, 'wrong card'
 
 def test_random_with_choice_option():
     card = rand.random_with_choice_and_sample_attribute()
     assert '10S' in card
+
+def test_original_sequence_shuffle():
+    shuffled = rand.retain_original_sequence_and_shuffle()
+    assert shuffled != 'AS'
---
+++ randomness/tests/test_scrambled_puzzle.py
@@ -0,0 +1,5 @@
+from randomness.src import scrambled_puzzle as scrambled_puzzle
+from randomness.src.scrambled_puzzle import words
+
+def test_scrambled_puzzle():
+    assert 'horse' in words
